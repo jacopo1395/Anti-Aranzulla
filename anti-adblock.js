3d486f89d2b3a6e7fb77c423f89a32fe8a54154f
Jacopo Carlini <jack1395@hotmail.it>
2018-10-02 13:01:50 +0200

    Update anti-adblock.js

--- anti-adblock.js
+++ anti-adblock.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  simple Anti-AntiADBlocker for aranzulla.it
-// @author       Jacopo Carlini
+// @author       Jacopo1395
 // @match        https://www.aranzulla.it/*
 // @grant        none
 // ==/UserScript==
@@ -13,8 +13,8 @@
 
     // Your code here...
     setTimeout(function () {
-       console.log(document.body.children[9]);
        var elem = document.body.children[9];
        document.body.removeChild(elem);
+       console.log("done!");
     }, 5000);
 })();
